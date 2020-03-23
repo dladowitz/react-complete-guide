6be7d23d624d7138cc3e74f1b9068393ec2d3ed3
David Ladowitz <david@ladowitz.com>
2020-03-23 10:10:42 -0700

    Doing things with lists and conditions

--- src/App.js
+++ src/App.js
@@ -1,35 +1,85 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-const app = (props) => {
-  const stateArr = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Stephanie', age: 26 },
       { name: 'Manu', age: 29 },
-    ]
-  });
+    ], 
+    otherState: 'some other value',
+    showNames: true,
+  };
 
+  toggleNameHandler = () => {
+    console.log("Calling toggleNameHandler")
+    this.setState({ showNames: !this.state.showNames })
+  }
 
-  return (
-    <div className="App">
-      <h1 onCopy={this.copyHandler}>Hi, I'm a React App</h1>
-      <button onClick={this.switchNameHandler}>Switch Name</button>
-      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobby is Rock Climbing</Person>
-    </div>
-  );
+  deleteNameHandler = (personIndex) => {
+    console.log("Calling deleteNameHandler")
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons })
+  }
+
+  nameChangeHandler = (event) => {
+    console.log("Calling nameChangeHandler");
+    let newPersons = this.state.persons;
+    console.log(event.target.id);
+    
+    newPersons[event.target.id].name = event.target.value;
+    this.setState({
+      persons: newPersons
+    }) 
+  }
+
+  copyHandler = () => {
+    console.log("calling copyHandler");
+  }
+
+  render () {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+    }
+
+    let persons = "Hiding Persons";
+    
+    if (this.state.showNames) {
+      persons = (
+        this.state.persons.map((person, index) => {
+          return <Person
+            key={index}
+            name={person.name}
+            age={person.age}
+            click={() => this.deleteNameHandler(index)}
+            changed={this.nameChangeHandler}
+          ></Person>
+        })
+      )
+    }
+
+    return (
+      <div className="App">
+        <h1 onCopy={this.copyHandler}>Hi, I'm a React App</h1>
+
+        <button onClick={() => this.toggleNameHandler('David')} style={style}>Toggle Names</button>
+        
+        <div>{persons}</div>
+      </div>
+    );
+  }
+
 }
 
-export default app;
+export default App;
 
 
 
-switchNameHandler = () => {
-  console.log("Calling switchHandler");
-  this.setState({ persons: [{ name: 'David', age: 43 }] })
-}
 
-copyHandler = () => {
-  console.log("calling copyHandler");
-}
